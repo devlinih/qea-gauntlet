9c5a58731f6132f412ee94e9f16d4bdfa1c42de7
Devlin Ih <dih@olin.edu>
2022-04-29 12:35:25 -0400

    Add pseudocode for level 1 challenge

---
+++ level1.m
@@ -0,0 +1,10 @@
+% Navigate the gauntlet challenge, level 1
+
+% Collect scan data, do not RANSAC fit it
+
+% Define the potential field of the gauntlet (using the pre-defined
+% values)
+
+% Start collecting path data
+
+% Follow gradient descent based on potential field
