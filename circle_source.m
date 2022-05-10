9691e4daef4d4af09a3b0c66753efd1a180a13f4
Devlin Ih <dih@olin.edu>
2022-05-09 23:39:41 -0400

    Clean up unused files

--- circle_source.m
+++
@@ -1,9 +0,0 @@
-function res = circle_source(x, y, hkr, u)
-    h = hkr(1);
-    k = hkr(2);
-    r = hkr(3);
-    theta = u * 2 * pi;
-    a = h + r * cos(theta);
-    b = k + r * sin(theta);
-    res = log(sqrt((x-a).^2 + (y-b).^2));
-end
--- flatland_assignment.m
+++ flatland_assignment.m
@@ -1,26 +1,40 @@
-function position = flatland_assignment()
+function scan_data = flatland_assignment()
+
+sub = rossubscriber('/scan');
+
 % to calculate wheel velocities for a given angular speed we need to know
 % the wheel base of the robot
 wheelBase = 0.235;              % meters
 % this is the scaling factor we apply to the gradient when calculating our
 % step size
-lambda = 0.1;
+lambda = 0.01;
 
 % setup symbolic expressions for the function and gradient
 syms x y;
 
-f = x*y - x^2 -y^2 - 2*x - 2*y + 4;
-%f = 16*exp(-((x^2)/2) - ((y^2)/2) - ((x*y)/2)) + 4*exp(-((x+1.5)^2)-((y+2.5)^2));
+f = 20*(log(sqrt((x-0.75).^2 + (y + 2.5).^2))) - 2*log(sqrt((x+0.25).^2 + (y+1).^2)) - 0.4*log(sqrt((x-1).^2 + (y+0.7).^2)) - 2.5*log(sqrt((x-1.41).^2 + (y+2).^2));
+
+% define sources at lines for walls
+% for a = -1.5:0.01:2.5
+%     f = f - 0.05*log(sqrt((x-a).^2 + (y-1).^2));
+%     f = f - 0.05*log(sqrt((x-a).^2 + (y+3.37).^2));
+% end
+% 
+% for b = -3.37:0.01:1
+%     f = f - 0.05*log(sqrt((x+1.5).^2 + (y-b).^2));
+%     f = f - 0.05*log(sqrt((x-2.5).^2 + (y-b).^2));
+% end
+
 grad = gradient(f, [x, y]);
 
 % the problem description tells us to the robot starts at position 1, -1
 % with a heading aligned to the y-axis
-heading = [0; 1];
+heading = [1; 0];
 %position = [0; +2];
-position = [1; -1];
+position = [0; 0];
 
 angularSpeed = 0.2;  % radians / second (set higher than real to help with testing)
-linearSpeed = 0.75;  % meters / second
+linearSpeed = 0.2;  % meters / second
 
 % get setup with a publisher so we can modulate the velocity
 pub = rospublisher('/raw_vel');
@@ -36,11 +50,21 @@ placeNeato(position(1), position(2), heading(1), heading(2));
 pause(2);
 
 % set a flag to control when we are sufficiently close to the maximum of f
-shouldStop = false;
+startime = rostic;
 
-while ~shouldStop
+% Initialise the return variable
+scan_data = []
+
+while rostoc(startime) < 19.1
+    
+    sample_num = size(scan_data, 2)
+    scan_message = receive(sub);
+    radius = scan_message.Ranges(1:end-1);
+    scan_data(:,sample_num+1) = [position; heading; radius]
+
+
     % get the gradient
-    gradValue = double(subs(grad, {x, y}, {position(1), position(2)}));
+    gradValue = -1*double(subs(grad, {x, y}, {position(1), position(2)}));
     % calculate the angle to turn to align the robot to the direction of
     % gradValue. There are lots of ways to do this. One way is to use the
     % fact that the magnitude of the cross product of two vectors is equal
@@ -89,7 +113,6 @@ while ~shouldStop
     % update the position for the next iteration
     position = position + (gradValue*lambda);
     % if our step is too short, flag it so we break out of our loop
-    shouldStop = forwardDistance < 0.01;
 end
 
 % stop the robot before exiting
